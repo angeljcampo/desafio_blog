fe1f0c27fd9f1a2598b631b90f51e1f789b47055
Angel <angeljcampo@gmail.com>
2021-05-13 16:34:45 -0400

    add views and validates

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :image_url, presence: true
+  validates :content, presence: true
+  before_save :censurate_word
+
+  def censurate_word
+  self.content = self.content.gsub("divulgar", "")
+  end
+end
